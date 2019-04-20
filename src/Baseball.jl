006d2ec5ab7efb74f25a7e204741c20945062028
Ben Elsen <mail@benelsen.com>
2019-04-20 15:39:18 +0200

    fetch and parse mlb game files

--- src/Baseball.jl
+++ src/Baseball.jl
@@ -1,9 +1,13 @@
 module Baseball
 
+using HTTP
+using DataFrames
+using EzXML
 using Dates
 using TimeZones
 
 include("utils.jl")
+include("data/mlb_game_files/mlb_game_files.jl")
 include("atmosphere.jl")
 
 end # module
---
+++ src/data/mlb_game_files/mlb_game_files.jl
@@ -0,0 +1,152 @@
+export get_games, get_weather_mlb, fetch_game_file
+
+function fetch_game_file(date::TimeType, basename::AbstractString)
+    year = Dates.format(date, "yyyy")
+    month = Dates.format(date, "mm")
+    day = Dates.format(date, "dd")
+
+    url = "https://gd2.mlb.com/components/game/mlb/year_$(year)/month_$(month)/day_$(day)/$(basename).xml"
+    req = HTTP.get(url, status_exception = true)
+    doc = parsexml(req.body)
+end
+
+function fetch_game_file(game_data_directory::AbstractString, basename::AbstractString)
+    url = "https://gd2.mlb.com$(game_data_directory)/$(basename).xml"
+    req = HTTP.get(url, status_exception = true)
+    doc = parsexml(req.body)
+end
+
+function fetch_game_file(date::TimeType, game_id::AbstractString, basename::AbstractString)
+    year = Dates.format(date, "yyyy")
+    month = Dates.format(date, "mm")
+    day = Dates.format(date, "dd")
+
+    game_data_directory = "/components/game/mlb/year_$(year)/month_$(month)/day_$(day)/$(game_id)"
+    fetch_game_file(game_data_directory, basename)
+end
+
+function fetch_game_file(date::TimeType, home_team_code::AbstractString, away_team_code::AbstractString, game_number::Integer, basename::AbstractString; home_sport_code = "mlb", away_sport_code = "mlb")
+    year = Dates.format(date, "yyyy")
+    month = Dates.format(date, "mm")
+    day = Dates.format(date, "dd")
+    game_id = "gid_$(year)_$(month)_$(day)_$(away_team_code)$(away_sport_code)_$(home_team_code)$(home_sport_code)_$(game_number)"
+
+    game_data_directory = "/components/game/mlb/year_$(year)/month_$(month)/day_$(day)/$(game_id)"
+    fetch_game_file(game_data_directory, basename)
+end
+
+game_types = [
+    :game_pk => Int64,
+    :game_type => Char,
+    :stats_season => Int64,
+    :time_date => ZonedDateTime,
+    :time => Time,
+    :ampm => String,
+    :time_zone => String,
+    :original_date => Date,
+    :resume_date => Union{Missing, Date},
+    :description => Union{Missing, String},
+    :location => String,
+    :venue => String,
+    :venue_id => Int64,
+    :venue_w_chan_loc => Union{Missing, String},
+    :league => String,
+    :away_code => String,
+    :away_division => Union{Missing, String},
+    :away_league_id => String,
+    :away_name_abbrev => String,
+    :away_sport_code => String,
+    :away_team_id => String,
+    :away_team_name => String,
+    :home_code => String,
+    :home_division => Union{Missing, String},
+    :home_league_id => String,
+    :home_name_abbrev => String,
+    :home_sport_code => String,
+    :home_team_id => String,
+    :home_team_name => String,
+    :double_header_sw => Char,
+    :game_nbr => Int64,
+    :scheduled_innings => Int64,
+    :series => String,
+    :ser_games => Union{Missing, Int64},
+    :ser_home_nbr => Union{Missing, Int64},
+    :series_num => Union{Missing, Int64},
+    :tbd_flag => Char,
+    :tiebreaker_sw => Char,
+    :game_data_directory => String,
+    :ind => Char,
+    :status => String
+]
+
+function get_games(date::TimeType = today())
+    doc = fetch_game_file(date, "master_scoreboard")
+
+    games = map(findall("/games/game", doc)) do game
+
+        status_node = findfirst("status", game)
+
+        ks = first.(game_types)
+        vals = map(ks[1:(end-2)]) do key
+            key_str = string(key)
+            haskey(game, key_str) ? game[key_str] : missing
+        end
+
+        push!(vals, status_node["ind"])
+        push!(vals, status_node["status"])
+
+        NamedTuple{(ks...,)}(vals)
+    end
+
+    if isempty(games)
+        DataFrame(last.(game_types), first.(game_types), 0)
+    else
+        d = DataFrame(games)
+        for name in names(d)
+            i = findfirst(isequal(name), first.(game_types))
+            T = last(game_types[i])
+            @info (T, name)
+            if T === Char || T === Union{Missing, Char}
+                d[name] = getindex.(d[name], 1)
+
+            elseif T <: Real
+                d[name] = parse.(Ref(T), d[name])
+
+            elseif T <: Union{Missing, Real}
+                d[name] = convert(Vector{T}, map(e -> isempty(e) ? missing : parse.(Ref(get_union_type_2(T)), e), d[name]))
+
+            elseif T === ZonedDateTime || T === Union{Missing, ZonedDateTime}
+                d[name] = create_time.(d[name], d[:ampm], tz"America/New_York")
+
+            elseif T === Time || T === Union{Missing, Time}
+                d[name] = Time.(d[name])
+
+            elseif T === Date || T === Union{Missing, Date}
+                d[name] = convert(Vector{T}, map(e -> isempty(e) ? missing : Date(e, dateformat"yyyy/mm/dd"), d[name]))
+
+            elseif T === String || T === Union{Missing, String}
+                d[name] = convert(Vector{T}, ifelse.(isempty.(d[name]), missing, d[name]))
+            else
+                # d[name] = convert.(Ref(T), d[name])
+            end
+        end
+        return d
+    end
+end
+
+function get_weather_mlb(game_pk::Integer, game_data_directory::AbstractString)
+    doc = fetch_game_file(game_data_directory, "plays")
+
+    node = findfirst("//weather", doc)
+    m_wind = match(r"(?<speed>\d+) ?mph (?<direction>[\w ]+)", node["wind"])
+
+    (
+        game_pk = game_pk,
+        inside = node["condition"] == "Dome" || node["condition"] == "Roof Closed",
+        cond = node["condition"],
+        temperature = round((parse(Float64, node["temp"]) - 32) * (5/9), digits = 1),
+        wind = node["wind"],
+        wind_speed = round(parse(Float64, m_wind[:speed]) * 0.44704, digits = 2),
+        wind_direction_str = m_wind[:direction]
+    )
+end
