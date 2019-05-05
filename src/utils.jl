47ae8169a8af52fbfe13ce275e584a5864e66b41
Ben Elsen <mail@benelsen.com>
2019-05-05 13:23:19 +0200

    fix for am/pm date parsing

--- src/utils.jl
+++ src/utils.jl
@@ -2,6 +2,9 @@
 get_union_type_2(::Type{Union{Missing, T}}) where T = T
 
 function create_time(time_str, ampm, tz)
+    if ismissing(time_str) || ismissing(ampm)
+        return missing
+    end
     dt = DateTime(time_str, dateformat"yyyy/mm/dd H:MM")
     dt += hour(dt) == 12 ? (ampm == "AM" ? Hour(12) : Hour(0)) : (ampm == "PM" ? Hour(12) : Hour(0))
     ZonedDateTime(dt, tz)
